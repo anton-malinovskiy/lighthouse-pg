b9916afac17205c77d369aa4ccce9d74faa12ca7
Antoni Malinowski <anton@Antonis-MacBook-Pro.local>
2020-08-13 10:52:01 +0200

    config update

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,5 +1,9 @@
 module.exports = {
     ci: {
+        collect: {
+            url: ['https://www.prettygreen.com'],
+            startServerCommand: 'rails server -e production',
+        },
         upload: {
             target: 'temporary-public-storage',
         },
